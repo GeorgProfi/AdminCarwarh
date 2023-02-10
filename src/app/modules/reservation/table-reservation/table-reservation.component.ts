dbb0238bbb98dcceacc69a7d9f39c3a91c082f57
Nikita <nik.oschepkov@yandex.ru>
2023-02-10 18:14:45 +0300

    прототип таблицы записей

--- src/app/modules/reservation/table-reservation/table-reservation.component.ts
+++ src/app/modules/reservation/table-reservation/table-reservation.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { ReservationService } from '../reservation.service';
 import { tuiCreateTimePeriods } from '@taiga-ui/kit';
+import { TuiAlertService } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-table-reservation',
@@ -9,21 +10,43 @@ import { tuiCreateTimePeriods } from '@taiga-ui/kit';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableReservationComponent {
-  constructor(private reservationService: ReservationService) {}
+  constructor(
+    private reservationService: ReservationService,
+    @Inject(TuiAlertService)
+    private readonly alertService: TuiAlertService
+  ) {}
 
   readonly times = tuiCreateTimePeriods();
 
   reserv = [
     [
-      { time: 1, duration: 1 },
+      { time: 0, duration: 1 },
+      { time: 4, duration: 2 },
       { time: 8, duration: 3 },
-      { time: 14, duration: 5 },
     ],
     [
       { time: 1, duration: 1 },
       { time: 2, duration: 1 },
     ],
+    [{ time: 0, duration: 1 }],
     [{ time: 1, duration: 1 }],
-    [{ time: 1, duration: 1 }],
+    [{ time: 1, duration: 8 }],
+    [{ time: 3, duration: 4 }],
+    [{ time: 5, duration: 8 }],
   ];
+
+  index = 0;
+
+  pageUp() {
+    ++this.index;
+  }
+
+  pageDown() {
+    --this.index;
+  }
+
+  clickOrder() {
+    console.log('OPANA!');
+    this.alertService.open('A simple option').subscribe();
+  }
 }
