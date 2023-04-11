a609c1e31bee9824671ab4876266b04d10bcdb09
Александр Серебренников <a.serebrenikov@mx.mdcm.co>
2023-04-11 15:12:36 +0200

    установка активного таба на странице auth

--- src/app/auth-page/auth-page.component.ts
+++ src/app/auth-page/auth-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,6 +7,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./auth-page.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthPageComponent {
+export class AuthPageComponent implements OnInit {
   activeItemIndex = 0;
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.setCurrentTab();
+  }
+
+  private setCurrentTab(): void {
+    if (this.router.url === '/auth/login') this.activeItemIndex = 0;
+    if (this.router.url === '/auth/register') this.activeItemIndex = 1;
+  }
 }
