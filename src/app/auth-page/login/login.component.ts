bb242cb8f1df4e301ed4664813cc51319d253e65
Александр Серебренников <a.serebrenikov@mx.mdcm.co>
2023-04-11 17:43:08 +0200

    рефакторинг страницы login

--- src/app/auth-page/login/login.component.ts
+++ src/app/auth-page/login/login.component.ts
@@ -1,108 +1,120 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
-import { TUI_PASSWORD_TEXTS, TUI_PROMPT, tuiInputPasswordOptionsProvider } from '@taiga-ui/kit';
-import { of } from 'rxjs';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TuiValidationError } from '@taiga-ui/cdk';
 import { AuthService } from '../../auth/auth.service';
-import { TuiAlertService, TuiDialogService } from '@taiga-ui/core';
+import { TuiDialogContext, TuiDialogService } from '@taiga-ui/core';
 import { AccountService } from '../../common/services/api/account.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PolymorpheusContent } from '@tinkoff/ng-polymorpheus';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    tuiInputPasswordOptionsProvider({
-      icons: {
-        hide: `tuiIconLockLarge`,
-        show: `tuiIconLockOpenLarge`,
-      },
-    }),
-    {
-      provide: TUI_PASSWORD_TEXTS,
-      useValue: of([``]),
-    },
-  ],
 })
 export class LoginComponent implements OnInit {
+  private error: TuiValidationError | null = null;
+  private refreshPasswordDialogSbs?: Subscription | null = null;
+  isLoading = false;
+
+  readonly form = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
   constructor(
     private route: Router,
     private authService: AuthService,
     private accountService: AccountService,
-    @Inject(TuiAlertService)
-    private readonly alertService: TuiAlertService,
-    @Inject(TuiDialogService) private readonly dialogService: TuiDialogService
+    private changeDetectionRef: ChangeDetectorRef,
+    private readonly dialogService: TuiDialogService,
   ) {}
+
+  get isInvalid(): boolean {
+    return this.form.invalid;
+  }
+
+  get email(): string | null | undefined {
+    return this.form.value.email;
+  }
+
+  get password(): string | null | undefined {
+    return this.form.value.password;
+  }
 
   ngOnInit() {
     const query = this.route.parseUrl(this.route.url).queryParams;
     if (query['reg_email']) {
       this.dialogService.open(`Почта успешно подтверждена!`).subscribe();
-      this.email = query['reg_email'];
+      this.form.get('email')?.setValue(query['reg_email']);
     } else if (query['refresh_pass_email']) {
       this.dialogService.open(`Пароль успешно обновлен!`).subscribe();
-      this.email = query['refresh_pass_email'];
+      this.form.get('email')?.setValue(query['refresh_pass_email']);
     } else if (query['change_email_email']) {
       this.dialogService.open(`Почта успешно обновлена!`).subscribe();
-      this.email = query['change_email_email'];
+      this.form.get('email')?.setValue(query['change_email_email']);
     }
   }
 
-  email = '';
-  password = '';
-
-  enabledError = false;
-  error = new TuiValidationError(`Неверно введен логин или пароль`);
-
   get computedError(): TuiValidationError | null {
-    return this.enabledError ? this.error : null;
+    return this.error || null;
+  }
+
+  private showError(error: string): void {
+    this.error = new TuiValidationError(error);
+    this.changeDetectionRef.detectChanges();
   }
 
   async login(): Promise<void> {
-    if (!this.email || !this.password) {
-      this.enabledError = true;
-      return;
+    if (this.email && this.password) {
+      this.authService.login(this.email, this.password).subscribe({
+        next: () => this.route.navigateByUrl(''),
+        error: res => this.showError(res.error.message),
+      });
     }
-
-    this.authService.login(this.email, this.password).subscribe({
-      next: data => {
-        this.route.navigateByUrl('');
-      },
-      error: error => {
-        this.enabledError = true;
-      },
-    });
   }
 
-  refreshPassword() {
-    if (!this.email || !this.password) {
-      this.enabledError = true;
-      return;
-    }
+  onShowRefreshPasswordDialog(content: PolymorpheusContent<TuiDialogContext>): void {
+    this.refreshPasswordDialogSbs = this.dialogService.open(content, {label: 'Вы уверены?'}).subscribe();
+  }
 
-    this.dialogService
-      .open<boolean>(TUI_PROMPT, {
-        label: 'Вы уверены?',
-        data: {
-          content: `На почту ${this.email} будет отправлено письмо с временным паролем`,
-        },
-        size: 's',
-        closeable: false,
-        dismissible: false,
-      })
-      .subscribe();
+  onPasswordRefresh(): void {
+    this.email && this.refreshPassword(this.email);
+  }
+
+  private refreshPasswordSuccess(): void {
+    this.refreshPasswordDialogSbs?.unsubscribe();
+    this.refreshPasswordDialogSbs = null;
+
+    this.dialogService.open('', {
+      label: 'Письмо успешно отправлено!',
+      size: 's',
+      closeable: false,
+      dismissible: false,
+    })
+    .subscribe();
+  }
+
+  private refreshPasswordError(): void {
+    this.dialogService.open('', {
+      label: 'Произошла ошибка, попробуйте позже!',
+      size: 's',
+      closeable: false,
+      dismissible: false,
+    })
+    .subscribe();
+  }
+
+  private refreshPassword(email: string) {
+    this.isLoading = true;
     this.accountService
-      .refreshPassword({
-        email: this.email,
+      .refreshPassword({email})
+      .subscribe({
+        next: () => this.refreshPasswordSuccess(),
+        error: () => this.refreshPasswordError(),
+        complete: () => this.isLoading = false,
       })
-      .subscribe(
-        () => {
-          console.log('ok');
-        },
-        error => {
-          console.error(error);
-        }
-      );
   }
 }
